4224bde7aa7fd6372121dac0a8e3b3f439012a5b
Emiliano Zarzoza Estrada <emilianozarzozaestrada@Emilianos-MacBook-Pro.local>
2019-05-28 15:10:27 -0500

    Second coounter functionallity added

---
+++ controllers/Controller1/build/release/Controller1.d
@@ -0,0 +1,4 @@
+build/release/Controller1.o: Controller1.c \
+  /Applications/Webots.app/include/controller/c/webots/robot.h \
+  /Applications/Webots.app/include/controller/c/webots/types.h \
+  /Applications/Webots.app/include/controller/c/webots/nodes.h
